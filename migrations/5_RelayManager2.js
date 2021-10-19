a676525cf791603ad587e588e3cca722a996bd58
coozebra <timnbil@hotmail.com>
2021-10-20 01:40:45 +0900

    Update migration

--- migrations/5_RelayManager2.js
+++ migrations/5_RelayManager2.js
@@ -2,12 +2,9 @@ const WIDO = artifacts.require('WIDO');
 const RelayManager2 = artifacts.require('RelayManager2');
 
 module.exports = async function (deployer) {
-  await deployer.deploy(WIDO);
-  const wido = await WIDO.deployed();
-
   await deployer.deploy(
     RelayManager2,
-    wido.address,
+    `0x21a97B14499C76731062a3f4c1Fd67CD04D62980`,
     500,
   );
 };
