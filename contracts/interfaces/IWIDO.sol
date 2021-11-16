7014b43fdfbc6c338f61b1a29dde9afb3cf176c5
coozebra <timnbil@hotmail.com>
2021-11-16 11:50:00 +0900

    Revert WIDO

--- contracts/interfaces/IWIDO.sol
+++ contracts/interfaces/IWIDO.sol
@@ -7,6 +7,12 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface IWIDO is IERC20 {
     function setRelayer(address newRelayer) external;
 
+    function renounceOwnership() external;
+
+    function transferOwnership(address _newOwner) external;
+
+    function acceptOwnership() external;
+
     function mint(
         address account,
         uint256 amount
