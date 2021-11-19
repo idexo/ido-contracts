1947601d7dc761f029ee3edbe02890dba423b39b
coozebra <timnbil@hotmail.com>
2021-11-20 02:22:59 +0900

    Update RelayManager2Secure

--- contracts/bridges/RelayManager2Secure.sol
+++ contracts/bridges/RelayManager2Secure.sol
@@ -1,38 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "../interfaces/IWIDO.sol";
-import "../lib/Ownable.sol";
+import "../lib/Operatorable.sol";
 
-contract RelayManager2Secure is Ownable, ReentrancyGuard {
+contract RelayManager2Secure is Operatorable {
   using SafeERC20 for IWIDO;
-
-  // ERC20Permit
-  struct PermitRequest {
-    uint256 nonce;
-    uint256 deadline;
-    uint8 v;
-    bytes32 r;
-    bytes32 s;
-  }
+  using ECDSA for bytes32;
 
   // Wrapped IDO token address
   IWIDO public wIDO;
   // Bridge wallet for collecting fees
   address public bridgeWallet;
-
+  // Signer check length threshold
   uint8 public threshold = 1;
+  // Signer count
   uint8 public signerLength;
-  uint256 public adminFee; // fixd amount in WIDO
+  // Fixed admin fee in WIDO
+  uint256 public adminFee;
   uint256 public adminFeeAccumulated;
 
   // address => signer status
   mapping(address => bool) private _signers;
-  // Transfer nonce
+  // address => transfer nonce
   mapping(address => uint256) public nonces;
   // transfer from address => nonce => processed status
   mapping(address => mapping(uint256 => bool)) public processedNonces;
@@ -41,9 +34,10 @@ contract RelayManager2Secure is Ownable, ReentrancyGuard {
   event Deposited(address indexed from, address indexed receiver, uint256 toChainId, uint256 amount, uint256 nonce);
   event Sent(address indexed receiver, uint256 indexed amount, uint256 indexed transferredAmount);
   event AdminFeeChanged(uint256 indexed adminFeeBps);
-  event EthReceived(address indexed sender, uint256 amount);
-  event AdminFeeWithdraw(address indexed receiver, uint256 amount);
   event BridgeWalletChanged(address indexed bridgeWallet);
+  event ThresholdChanged(uint8 threshold);
+  event SignerAdded(address indexed signer);
+  event SignerRemoved(address indexed signer);
 
   constructor(
     IWIDO _wIDO,
@@ -55,7 +49,6 @@ contract RelayManager2Secure is Ownable, ReentrancyGuard {
     require(_adminFee != 0, "RelayManager2Secure: ADMIN_FEE_INVALID");
     require(_threshold >= 1, "RelayManager2Secure: THRESHOLD_INVALID");
     require(_bridgeWallet != address(0), "RelayManager2Secure: BRIDGE_WALLET_ADDRESS_INVALID");
-    threshold = _threshold;
 
     for (uint8 i = 0; i < signers_.length; i++) {
       if (signers_[i] != address(0) && !_signers[signers_[i]]) {
@@ -70,10 +63,7 @@ contract RelayManager2Secure is Ownable, ReentrancyGuard {
     wIDO = _wIDO;
     adminFee = _adminFee;
     bridgeWallet = _bridgeWallet;
-  }
-
-  receive() external payable {
-    emit EthReceived(msg.sender, msg.value);
+    threshold = _threshold;
   }
 
   /**************************|
@@ -82,31 +72,46 @@ contract RelayManager2Secure is Ownable, ReentrancyGuard {
 
   /**
     * @dev Set admin fee
-    * Only `owner` can call
     */
-  function setAdminFee(uint256 newAdminFee) external onlyOwner {
+  function setAdminFee(
+    uint256 newAdminFee,
+    bytes[] calldata signatures
+  ) external onlyOperator {
     require(newAdminFee != 0, "RelayManager2Secure: ADMIN_FEE_INVALID");
+    require(
+      _verify(keccak256(abi.encodePacked(newAdminFee)), signatures),
+      "RelayManager2Secure: INVALID_SIGNATURE"
+    );
     adminFee = newAdminFee;
 
     emit AdminFeeChanged(newAdminFee);
   }
 
-  function changeThreshold(
+  /**
+    * @dev Set threshold
+    */
+  function setThreshold(
     uint8 newThreshold,
     bytes[] calldata signatures
-  ) external {
+  ) external onlyOperator {
     require(newThreshold >= 1, "RelayManager2Secure: THRESHOLD_INVALID");
     require(
       _verify(keccak256(abi.encodePacked(newThreshold)), signatures),
       "RelayManager2Secure: INVALID_SIGNATURE"
     );
     threshold = newThreshold;
+
+    emit ThresholdChanged(newThreshold);
   }
 
+  /**
+    * @dev Add new signer
+    * `signer` must not be zero address
+    */
   function addSigner(
     address signer,
     bytes[] calldata signatures
-  ) external {
+  ) external onlyOperator {
     require(
       _verify(keccak256(abi.encodePacked(signer)), signatures),
       "RelayManager2Secure: INVALID_SIGNATURE"
@@ -116,12 +121,17 @@ contract RelayManager2Secure is Ownable, ReentrancyGuard {
       _signers[signer] = true;
       signerLength++;
     }
+
+    emit SignerAdded(signer);
   }
 
+  /**
+    * @dev Remove signer
+    */
   function removeSigner(
     address signer,
     bytes[] calldata signatures
-  ) external {
+  ) external onlyOperator {
     require(
       _verify(keccak256(abi.encodePacked(signer)), signatures),
       "RelayManager2Secure: INVALID_SIGNATURE"
@@ -130,13 +140,22 @@ contract RelayManager2Secure is Ownable, ReentrancyGuard {
     if (_signers[signer] && --signerLength >= threshold) {
       _signers[signer] = false;
     }
+
+    emit SignerRemoved(signer);
   }
 
   /**
     * @dev Set bridge wallet address for collecting admin fees
    */
-  function setBridgeWallet(address newBridgeWallet) external onlyOwner {
+  function setBridgeWallet(
+    address newBridgeWallet,
+    bytes[] calldata signatures
+  ) external onlyOperator {
       require(newBridgeWallet != address(0), "RelayManager2Secure: BRIDGE_WALLET_ADDRESS_INVALID");
+      require(
+        _verify(keccak256(abi.encodePacked(newBridgeWallet)), signatures),
+        "RelayManager2Secure: INVALID_SIGNATURE"
+      );
       bridgeWallet = newBridgeWallet;
 
       emit BridgeWalletChanged(newBridgeWallet);
@@ -171,29 +190,9 @@ contract RelayManager2Secure is Ownable, ReentrancyGuard {
     uint256 amount,
     uint256 nonce,
     bytes[] calldata signatures
-  ) external nonReentrant {
+  ) external onlyOperator {
     _send(from, receiver, amount, nonce, signatures);
   }
-
-  /**********************|
-  |          Fee         |
-  |_____________________*/
-
-  // /**
-  //   * @dev Withdraw admin fee accumulated
-  //   * Only `owner` can call
-  //   */
-  // function withdrawAdminFee(
-  //   uint256 amount
-  // ) external onlyOwner {
-  //   require(bridgeWallet != address(0), "RelayManager2Secure: BRIDGE_WALLET_ADDRESS_INVALID");
-  //   require(amount > 0, "RelayManager2Secure: AMOUNT_INVALID");
-  //   require(adminFeeAccumulated >= amount, "RelayManager2Secure: INSUFFICIENT_ADMIN_FEE");
-  //   adminFeeAccumulated -= amount;
-  //   wIDO.safeTransfer(bridgeWallet, amount);
-
-  //   emit AdminFeeWithdraw(bridgeWallet, amount);
-  // }
 
   function _send(
     address from,
@@ -222,6 +221,9 @@ contract RelayManager2Secure is Ownable, ReentrancyGuard {
     emit Sent(receiver, amount, amountToTransfer);
   }
 
+  /**
+    * @dev Check signer status of `_candidate`
+   */
   function isSigner(address _candidate) public view returns (bool) {
     return _signers[_candidate];
   }
@@ -230,12 +232,12 @@ contract RelayManager2Secure is Ownable, ReentrancyGuard {
     bytes32 _hash,
     bytes[] memory _signatures
   ) private view returns (bool) {
-    bytes32 h = ECDSA.toEthSignedMessageHash(_hash);
+    bytes32 h = _hash.toEthSignedMessageHash();
     address lastSigner = address(0x0);
     address currentSigner;
 
     for (uint256 i = 0; i < _signatures.length; i++) {
-      currentSigner = ECDSA.recover(h, _signatures[i]);
+      currentSigner = h.recover( _signatures[i]);
 
       if (currentSigner <= lastSigner) {
         return false;
