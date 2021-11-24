d317c943f2bfb884ad45d7cd0784b2f9253bcf84
Helder Sepulveda <heldersepu@gmail.com>
2021-11-24 08:33:29 -0500

    withdrawAdminFee was removed

--- test/RelayManagerETH.test.js
+++ test/RelayManagerETH.test.js
@@ -121,15 +121,6 @@ contract('RelayManagerETH', async accounts => {
         expect(res.toString()).to.eq(receiveAmount.toString());
       });
     });
-    it('withdrawAdminFee', async () => {
-      expectEvent(
-        await relayManager.withdrawAdminFee(carol, adminFee),
-        'AdminFeeWithdraw'
-      );
-      await relayManager.adminFeeAccumulated().then(res => {
-        expect(res.toString()).to.eq('0');
-      });
-    });
     it('withdrawGasFee', async () => {
       expectEvent(
         await relayManager.withdrawGasFee(carol, gasFee),
