87e0ff88caef1ca39ea3b92cdc0dc7e73c858fa5
coozebra <timnbil@hotmail.com>
2021-12-16 12:05:49 +0900

    Add stake coupon mechanism

--- contracts/PriceStabilityPool.sol
+++ contracts/PriceStabilityPool.sol
@@ -34,7 +34,15 @@ contract PriceStabilityPool is ERC721Enumerable, Operatorable, Whitelist, Reentr
   // Gas price in stable coin
   uint256 public couponStablePrice;
   // Last stake NFT id, start from 1
-  uint256 public stakeId;
+  uint256 public lastStakeId;
+  // First stake NFT id, start from 1
+  uint256 public firstStakeId = 1;
+  // Total staked coupon amount
+  uint256 public totalCoupon;
+  // Entrance fee in basis point, max 1000
+  uint256 public entranceFeeBP;
+  // Staker wallet array
+  address[] public stakers;
 
   // Access ticket NFT structure
   struct TicketInfo {
@@ -42,14 +50,18 @@ contract PriceStabilityPool is ERC721Enumerable, Operatorable, Whitelist, Reentr
     uint256 duration;
   }
 
-  // stake NFT id => created coupon amount
+  // Stake NFT id => created coupon amount
   mapping(uint256 => uint256) public stakedCoupons;
+  // Staker address => total created coupon amount
+  mapping(address => uint256) public couponBalances;
   // Wallet address => ticket info
   mapping(address => TicketInfo) public tickets;
   // Ticket type hash => price in WIDO
   mapping(bytes32 => uint256) public ticketPrices;
   // Wallet address => purchased coupon amount
   mapping(address => uint256) public purchasedCoupons;
+  // Staker address => premium (entrance fee)
+  mapping(address => uint256) public premiums;
 
   constructor(
     string memory _name,
@@ -59,18 +71,21 @@ contract PriceStabilityPool is ERC721Enumerable, Operatorable, Whitelist, Reentr
     uint256 _stabilityPeriod,
     uint256 _stabilityStartTime,
     uint256 _couponGasPrice,
-    uint256 _couponStablePrice
+    uint256 _couponStablePrice,
+    uint256 _entranceFeeBP
   ) ERC721(_name, _symbol) {
     require(address(_wido) != address(0), "PriceStabilityPool: WIDO_ADDRESS_INVALID");
     require(address(_usdt) != address(0), "PriceStabilityPool: USDT_ADDRESS_INVALID");
     require(_stabilityPeriod != 0, "PriceStabilityPool: STABILITY_PERIOD_INVALID");
     require(_stabilityStartTime >= block.timestamp, "PriceStabilityPool: STABILITY_START_TIME_INVALID");
+    require(_entranceFeeBP <= 1000, "PriceStabilityPool: ENTRANCE_FEE_INVALID");
     wido = _wido;
     usdt = _usdt;
     stabilityPeriod = _stabilityPeriod;
     couponGasPrice = _couponGasPrice;
     couponStablePrice = _couponStablePrice;
     stabilityStartTime = _stabilityStartTime;
+    entranceFeeBP = _entranceFeeBP;
   }
 
   /**
@@ -121,11 +136,15 @@ contract PriceStabilityPool is ERC721Enumerable, Operatorable, Whitelist, Reentr
     require(whitelist[msg.sender], "PriceStabilityPool: CALLER_NO_WHITELIST");
     require(_amount != 0, "PriceStabilityPool: COUPON_AMOUNT_INVALID");
     require(msg.value >= _amount * couponGasPrice, "PriceStabilityPool: INSUFFICIENT_FUNDS");
-    require(block.timestamp > stabilityStartTime, "PriceStabilityPool: STABILITY_PERIOD_NOT_STARTED");
     require(block.timestamp <= stabilityStartTime + stabilityPeriod, "PriceStabilityPool: STABILITY_PERIOD_ENDED");
-    uint256 newId = ++stakeId;
+    uint256 newId = ++lastStakeId;
     super._mint(msg.sender, newId);
     stakedCoupons[newId] = _amount;
+    if (couponBalances[msg.sender] == 0) {
+      stakers.push(msg.sender);
+    }
+    couponBalances[msg.sender] += _amount;
+    totalCoupon += _amount;
     // return change if any
     uint256 change = msg.value - _amount * couponGasPrice;
     if (change > 0) {
@@ -140,9 +159,11 @@ contract PriceStabilityPool is ERC721Enumerable, Operatorable, Whitelist, Reentr
    */
   // TODO check multiple ticket purchase or ticket update
   function purchaseTicket(bytes32 _ticketHash) external {
+    require(block.timestamp <= stabilityStartTime + stabilityPeriod, "PriceStabilityPool: STABILITY_PERIOD_ENDED");
     uint256 ticketPrice = ticketPrices[_ticketHash];
     require(ticketPrice != 0, "PriceStabilityPool: TICKET_HASH_INVALID");
-    wido.safeTransferFrom(msg.sender, address(this), ticketPrice);
+    uint256 entranceFee = ticketPrice * entranceFeeBP / 10000;
+    wido.safeTransferFrom(msg.sender, address(this), ticketPrice * (10000 + entranceFeeBP) / 10000);
     require(tickets[msg.sender].startTime == 0, "PriceStabilityPool: CALLER_HAS_ALREADY_TICKET");
     tickets[msg.sender].startTime = block.timestamp;
     if (_ticketHash == ONE_TIME_TICKET_HASH) {
@@ -160,6 +181,7 @@ contract PriceStabilityPool is ERC721Enumerable, Operatorable, Whitelist, Reentr
       // TODO check again
       tickets[msg.sender].duration = 0;
     }
+    _distributeEntranceFee(entranceFee);
   }
 
   /**
@@ -167,14 +189,66 @@ contract PriceStabilityPool is ERC721Enumerable, Operatorable, Whitelist, Reentr
    * TODO check burning expired tickets
    */
   function purchaseCoupon(uint256 _amount) external {
-    require(_amount != 0, "PriceStabilityPool: COUPON_AMOUNT_INVALID");
+    require(block.timestamp <= stabilityStartTime + stabilityPeriod, "PriceStabilityPool: STABILITY_PERIOD_ENDED");
+    require(_amount != 0 && _amount <= totalCoupon, "PriceStabilityPool: COUPON_AMOUNT_INVALID");
     TicketInfo memory ticket = tickets[msg.sender];
+    // TODO check time manage again
     require(ticket.startTime != 0, "PriceStabilityPool: ACCESS_TICKET_INVALID");
     require(ticket.duration <= 1 || (ticket.duration > 1 && ticket.startTime + ticket.duration >= block.timestamp), "PriceStabilityPool: ACCESS_TICKET_INVALID");
-    usdt.safeTransferFrom(msg.sender, address(this), _amount * couponStablePrice);
+
+    while(_amount > 0) {
+      uint256 firstStakeAmount = stakedCoupons[firstStakeId];
+      if (firstStakeAmount > _amount) {
+        // TODO check fee here
+        usdt.safeTransferFrom(msg.sender, ownerOf(firstStakeId), _amount * couponStablePrice);
+        stakedCoupons[firstStakeId] -= _amount;
+        _amount = 0;
+      } else {
+        address firstStaker = ownerOf(firstStakeId);
+        // TODO check fee here
+        usdt.safeTransferFrom(msg.sender, firstStaker, firstStakeAmount * couponStablePrice);
+        _burn(firstStakeId);
+        totalCoupon -= firstStakeAmount;
+        couponBalances[firstStaker] -= firstStakeAmount;
+        // remove first staker from `stakers` array
+        if (couponBalances[firstStaker] == 0) {
+          for (uint256 i = 0; i < stakers.length; i++) {
+            if (stakers[i] == firstStaker) {
+              stakers[i] = stakers[stakers.length - 1];
+              stakers.pop();
+              break;
+            }
+          }
+        }
+        firstStakeId++;
+        _amount -= firstStakeAmount;
+      }
+    }
+
     // delete one-time access ticket
     if (ticket.duration == 1) {
       delete tickets[msg.sender];
+    }
+  }
+
+  /**
+   * @dev Claim premium
+   * `_amount` must not be zero and less than premium
+   */
+  function claim(uint256 _amount) external nonReentrant {
+    require(_amount != 0 && _amount <= premiums[msg.sender], "PriceStabilityPool: CLAIM_AMOUNT_INVALID");
+    wido.safeTransfer(msg.sender, _amount);
+    premiums[msg.sender] -= _amount;
+  }
+
+  /**
+   * @dev Distribute entrance fee from access ticket purchase to stake holders
+   */
+  function _distributeEntranceFee(uint256 _fee) private {
+    require(_fee != 0, "PriceStabilityPool: INVALID_FEE");
+    for (uint256 i = 0; i < stakers.length; i++) {
+      address staker = stakers[i];
+      premiums[staker] += _fee * couponBalances[staker] / totalCoupon;
     }
   }
 }
