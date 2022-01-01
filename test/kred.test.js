57ae7f71ee51e61777c3ccc6a3ca75ba0bdb19cb
Helder Sepulveda <heldersepu@gmail.com>
2022-01-01 17:15:21 -0500

    Correction on FIDO

--- test/kred.test.js
+++ test/kred.test.js
@@ -2,5 +2,5 @@ const {testToken} = require('./kred');
 
 testToken('KRED', 'contracts/token/KRED.sol:KRED');
 testToken('PIDO', 'contracts/token/PIDO.sol:PIDO');
-testToken('FIDO', 'contracts/token/PIDO.sol:FIDO');
+testToken('FIDO', 'contracts/token/FIDO.sol:FIDO');
 testToken('WIDO', 'contracts/token/WIDO.sol:WIDO');
