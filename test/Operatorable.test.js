d0212d67855f5d676b68883dc3ef5b7ad1694e2d
Helder Sepulveda <heldersepu@gmail.com>
2022-01-02 18:29:55 -0500

    Testing Operatorable

---
+++ test/Operatorable.test.js
@@ -0,0 +1,37 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe('Operatorable', async () => {
+  let contract, alice, bob;
+
+  before(async () => {
+    [alice, bob] = await ethers.getSigners();
+    contract = await (await ethers.getContractFactory('Operatorable')).deploy();
+  });
+
+  describe('addOperator()', async () => {
+    it('expect to add operator', async () => {
+      await contract.addOperator(alice.address)
+      expect(await contract.checkOperator(alice.address)).to.eq(true);
+    });
+    describe('reverts if', async () => {
+      it('non owner call', async () => {
+        await expect(contract.connect(bob).addOperator(bob.address))
+          .to.be.revertedWith('Ownable: CALLER_NO_OWNER');
+      });
+    });
+  });
+
+  describe('removeOperator()', async () => {
+    it('expect to remove operator', async () => {
+      await contract.removeOperator(alice.address)
+      expect(await contract.checkOperator(alice.address)).to.eq(false);
+    });
+    describe('reverts if', async () => {
+      it('non owner call', async () => {
+        await expect(contract.connect(bob).removeOperator(bob.address))
+          .to.be.revertedWith('Ownable: CALLER_NO_OWNER');
+      });
+    });
+  });
+});
