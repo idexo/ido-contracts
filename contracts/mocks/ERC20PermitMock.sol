4c4b74fc72ac254b2ec85c124c7c34e66ecd3ce8
Helder Sepulveda <heldersepu@gmail.com>
2022-02-18 21:30:40 -0500

    Format the ERC20Mock

--- contracts/mocks/ERC20PermitMock.sol
+++ contracts/mocks/ERC20PermitMock.sol
@@ -5,28 +5,21 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract ERC20PermitMock is ERC20Permit {
-    constructor(
-        string memory name_,
-        string memory symbol_
-    ) ERC20(name_, symbol_) ERC20Permit(name_) {}
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) ERC20Permit(name_) {}
 
-    function mint(
-        address account,
-        uint256 amount
-    ) public {
+    function mint(address account, uint256 amount) public {
         super._mint(account, amount);
     }
 
-    function burn(
-        address account,
-        uint256 amount
-    ) public {
+    function burn(address account, uint256 amount) public {
         super._burn(account, amount);
     }
 
     function getChainId() public view returns (uint256) {
         uint256 id;
-        assembly { id := chainid() }
+        assembly {
+            id := chainid()
+        }
         return id;
     }
 }
