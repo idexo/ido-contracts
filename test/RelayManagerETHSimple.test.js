d8e6ab462788a7a92ac0a31d1956c95f5f8390bb
Diogo Oliveira <dluks82@gmail.com>
2022-02-21 17:20:41 -0300

    emit EthReceived test on RelayManagerETHSimple

--- test/RelayManagerETHSimple.test.js
+++ test/RelayManagerETHSimple.test.js
@@ -1,203 +1,156 @@
 // Initiate `ownerPrivateKey` with the third account private key on test evm
 
-const { expect } = require('chai');
-const {
-  BN,
-  constants,
-  expectEvent,
-  expectRevert
-} = require('@openzeppelin/test-helpers');
-const {
-  PERMIT_TYPEHASH,
-  getPermitDigest,
-  getDomainSeparator,
-  sign
-} = require('./helpers/signature');
+const { expect } = require("chai")
+const { BN, constants, expectEvent, expectRevert } = require("@openzeppelin/test-helpers")
+const { PERMIT_TYPEHASH, getPermitDigest, getDomainSeparator, sign } = require("./helpers/signature")
 
-const RelayManagerETH = artifacts.require('RelayManagerETHSimple');
-const ERC20PermitMock = artifacts.require('ERC20PermitMock');
+const RelayManagerETH = artifacts.require("RelayManagerETHSimple")
+const ERC20PermitMock = artifacts.require("ERC20PermitMock")
 
-contract('RelayManagerETH', async accounts => {
-  let relayManager;
-  let ido;
-  const [alice, bob, carol, bridge] = accounts;
-  const idoName = 'Idexo Token';
-  const idoSymbol = 'IDO';
-  // const ownerPrivateKey = Buffer.from('08c83289b1b8cce629a1e58b65c25b1c8062d5c9ec6375dc8265ad13ba25c630', 'hex');
-  const ownerPrivateKey = Buffer.from(
-    "7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
-    "hex"
-  );
+contract("RelayManagerETH", async (accounts) => {
+    let relayManager
+    let ido
+    const [alice, bob, carol, bridge] = accounts
+    const idoName = "Idexo Token"
+    const idoSymbol = "IDO"
+    // const ownerPrivateKey = Buffer.from('08c83289b1b8cce629a1e58b65c25b1c8062d5c9ec6375dc8265ad13ba25c630', 'hex');
+    const ownerPrivateKey = Buffer.from("7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6", "hex")
 
-  before(async () => {
-    ido = await ERC20PermitMock.new(idoName, idoSymbol);
-    relayManager = await RelayManagerETH.new(ido.address, new BN(30));
+    before(async () => {
+        ido = await ERC20PermitMock.new(idoName, idoSymbol)
+        relayManager = await RelayManagerETH.new(ido.address, new BN(30))
 
-    ido.mint(alice, web3.utils.toWei(new BN(1000)));
-    ido.mint(carol, web3.utils.toWei(new BN(1000)));
-    await ido.approve(relayManager.address, web3.utils.toWei(new BN(1000)), {from: alice});
-  });
+        ido.mint(alice, web3.utils.toWei(new BN(1000)))
+        ido.mint(carol, web3.utils.toWei(new BN(1000)))
+        await ido.approve(relayManager.address, web3.utils.toWei(new BN(1000)), { from: alice })
+    })
 
-  describe('#Role', async () => {
-    it ('should add operator', async () => {
-      await relayManager.addOperator(bob);
-      expect(await relayManager.checkOperator(bob)).to.eq(true);
-    });
-    it('should remove operator', async () => {
-      await relayManager.removeOperator(bob);
-      expect(await relayManager.checkOperator(bob)).to.eq(false);
-    });
-    describe('reverts if', async () => {
-      it('add operator by non-admin', async () => {
-        await expectRevert(
-          relayManager.addOperator(bob, {from: bob}),
-          'RelayManagerETH: CALLER_NO_OWNER'
-        );
-      });
-      it('remove operator by non-admin', async () => {
-        await relayManager.addOperator(bob);
-        await expectRevert(
-          relayManager.removeOperator(bob, {from: bob}),
-          'RelayManagerETH: CALLER_NO_OWNER'
-        );
-      });
-    });
-  });
+    describe("#Role", async () => {
+        it("should add operator", async () => {
+            await relayManager.addOperator(bob)
+            expect(await relayManager.checkOperator(bob)).to.eq(true)
+        })
+        it("should remove operator", async () => {
+            await relayManager.removeOperator(bob)
+            expect(await relayManager.checkOperator(bob)).to.eq(false)
+        })
+        describe("reverts if", async () => {
+            it("add operator by non-admin", async () => {
+                await expectRevert(relayManager.addOperator(bob, { from: bob }), "RelayManagerETH: CALLER_NO_OWNER")
+            })
+            it("remove operator by non-admin", async () => {
+                await relayManager.addOperator(bob)
+                await expectRevert(relayManager.removeOperator(bob, { from: bob }), "RelayManagerETH: CALLER_NO_OWNER")
+            })
+        })
+    })
 
-  describe('cross-chain transfer', async () => {
-    let adminFee, gasFee, receiveAmount;
-    const sendAmount = web3.utils.toWei(new BN(100));
-    const dummyDepositHash = '0xf408509b00caba5d37325ab33a92f6185c9b5f007a965dfbeff7b81ab1ec871b';
-    const polygonChainId = new BN(137);
+    describe("#Fund contract", async () => {
+        it("should fund contract and emit event", async () => {
+            const value = web3.utils.toWei(new BN(1), "ether")
+            expectEvent(await relayManager.sendTransaction({ value: value }), "EthReceived")
+            // instance provider
+            const provider = waffle.provider
+            const balance = await provider.getBalance(relayManager.address)
+            expect(balance.toString()).to.eq(value.toString())
+        })
+    })
 
-    it('deposit', async () => {
-      // Start transfer to Polygon (alice => bob)
-      expectEvent(
-        await relayManager.deposit(bob, sendAmount, polygonChainId, {from: alice}),
-        'Deposited'
-      );
-      await ido.balanceOf(relayManager.address).then(res => {
-        expect(res.toString()).to.eq('100000000000000000000');
-      });
-    });
-    it('permitAndDeposit', async () => {
-      let chainId;
-      await ido.getChainId().then(res => {
-        chainId = res.toNumber();
-      });
-      // Create the approval request
-      const approve = {
-        owner: carol,
-        spender: relayManager.address,
-        value: 100,
-      };
-      // deadline as much as you want in the future
-      const deadline = 100000000000000;
-      // Get the user's nonce
-      const nonce = await ido.nonces(carol);
-      // Get the EIP712 digest
-      const digest = getPermitDigest(idoName, ido.address, chainId, approve, nonce.toNumber(), deadline);
-      // Sign it
-      // NOTE: Using web3.eth.sign will hash the message internally again which
-      // we do not want, so we're manually signing here
-      const { v, r, s } = sign(digest, ownerPrivateKey);
-      const permitOptions = { nonce: nonce.toNumber(), deadline, v, r, s };
+    describe("cross-chain transfer", async () => {
+        let adminFee, gasFee, receiveAmount
+        const sendAmount = web3.utils.toWei(new BN(100))
+        const dummyDepositHash = "0xf408509b00caba5d37325ab33a92f6185c9b5f007a965dfbeff7b81ab1ec871b"
+        const polygonChainId = new BN(137)
 
-      // Approve it
-      expectEvent(
-        await relayManager.permitAndDeposit(bob, 100, polygonChainId, permitOptions, { from: carol }),
-        'Deposited'
-      );
-    });
-    it('send', async () => {
-      // Accept cross-chain transfer from Polygon (carol => bob)
+        it("deposit", async () => {
+            // Start transfer to Polygon (alice => bob)
+            expectEvent(await relayManager.deposit(bob, sendAmount, polygonChainId, { from: alice }), "Deposited")
+            await ido.balanceOf(relayManager.address).then((res) => {
+                expect(res.toString()).to.eq("100000000000000000000")
+            })
+        })
+        it("permitAndDeposit", async () => {
+            let chainId
+            await ido.getChainId().then((res) => {
+                chainId = res.toNumber()
+            })
+            // Create the approval request
+            const approve = {
+                owner: carol,
+                spender: relayManager.address,
+                value: 100
+            }
+            // deadline as much as you want in the future
+            const deadline = 100000000000000
+            // Get the user's nonce
+            const nonce = await ido.nonces(carol)
+            // Get the EIP712 digest
+            const digest = getPermitDigest(idoName, ido.address, chainId, approve, nonce.toNumber(), deadline)
+            // Sign it
+            // NOTE: Using web3.eth.sign will hash the message internally again which
+            // we do not want, so we're manually signing here
+            const { v, r, s } = sign(digest, ownerPrivateKey)
+            const permitOptions = { nonce: nonce.toNumber(), deadline, v, r, s }
 
-      expectEvent(
-        await relayManager.send(bob, sendAmount, dummyDepositHash, 1, {from: bob}),
-        'Sent'
-      );
-      adminFee = await relayManager.adminFeeAccumulated();
-      expect(adminFee.toString()).to.eq('300000000000000000');
-      gasFee = await relayManager.gasFeeAccumulated();
-      receiveAmount = sendAmount.sub(adminFee).sub(gasFee);
-      await ido.balanceOf(bob).then(res => {
-        expect(res.toString()).to.eq(receiveAmount.toString());
-      });
-    });
-    it('withdraw admin fee', async () => {
-      expectEvent(
-        await relayManager.withdrawAdminFee(bob, adminFee),
-        'AdminFeeWithdraw'
-      );
-    });
-    it('withdraw gas fee', async () => {
-      expectEvent(
-        await relayManager.withdrawGasFee(bob, gasFee),
-        'GasFeeWithdraw'
-      );
-    });
-    it('setMinTransferAmount', async () => {
-        await relayManager.setBaseGas(111);
-        expect((await relayManager.baseGas()).toString()).to.eq("111");
-      });
-    it('setMinTransferAmount', async () => {
-      await relayManager.setMinTransferAmount(112);
-      expect((await relayManager.minTransferAmount()).toString()).to.eq("112");
-    });
-  });
+            // Approve it
+            expectEvent(await relayManager.permitAndDeposit(bob, 100, polygonChainId, permitOptions, { from: carol }), "Deposited")
+        })
+        it("send", async () => {
+            // Accept cross-chain transfer from Polygon (carol => bob)
 
-  describe('#Ownership', async () => {
-    it('should transfer ownership', async () => {
-      await relayManager.transferOwnership(bob);
-      await relayManager.acceptOwnership({from: bob});
-      expect(await relayManager.owner()).to.eq(bob);
-      expectEvent(
-        await relayManager.setAdminFee(1, {from: bob}),
-        'AdminFeeChanged'
-      )
-    });
-    describe('reverts if', async () => {
-      it('non-owner call setMinTransferAmount', async () => {
-        await expectRevert(
-          relayManager.setMinTransferAmount(1, {from: carol}),
-          'RelayManagerETH: CALLER_NO_OWNER'
-        );
-      });
-      it('non-owner call setAdminFee', async () => {
-        await expectRevert(
-          relayManager.setAdminFee(1, {from: carol}),
-          'RelayManagerETH: CALLER_NO_OWNER'
-        );
-      });
-      it('non-owner call transferOwnership', async () => {
-        await expectRevert(
-          relayManager.transferOwnership(bob, {from: carol}),
-          'RelayManagerETH: CALLER_NO_OWNER'
-        );
-      });
-      it('call transferOwnership with zero address', async () => {
-        await expectRevert(
-          relayManager.transferOwnership(constants.ZERO_ADDRESS, {from: bob}),
-          'RelayManagerETH: INVALID_ADDRESS'
-        );
-      });
-      it('non owner call renounceOwnership', async () => {
-        await expectRevert(
-            relayManager.renounceOwnership({from: carol}),
-          'RelayManagerETH: CALLER_NO_OWNER'
-        );
-      });
-      it('non new owner call acceptOwnership', async () => {
-        await relayManager.transferOwnership(alice, {from: bob});
-        await expectRevert(
-          relayManager.acceptOwnership({from: carol}),
-          'RelayManagerETH: CALLER_NO_NEW_OWNER'
-        );
-        expectEvent(
-          await relayManager.renounceOwnership({from: bob}),
-          'OwnershipTransferred'
-        )
-      })
-    });
-  });
-});
+            expectEvent(await relayManager.send(bob, sendAmount, dummyDepositHash, 1, { from: bob }), "Sent")
+            adminFee = await relayManager.adminFeeAccumulated()
+            expect(adminFee.toString()).to.eq("300000000000000000")
+            gasFee = await relayManager.gasFeeAccumulated()
+            receiveAmount = sendAmount.sub(adminFee).sub(gasFee)
+            await ido.balanceOf(bob).then((res) => {
+                expect(res.toString()).to.eq(receiveAmount.toString())
+            })
+        })
+        it("withdraw admin fee", async () => {
+            expectEvent(await relayManager.withdrawAdminFee(bob, adminFee), "AdminFeeWithdraw")
+        })
+        it("withdraw gas fee", async () => {
+            expectEvent(await relayManager.withdrawGasFee(bob, gasFee), "GasFeeWithdraw")
+        })
+        it("setMinTransferAmount", async () => {
+            await relayManager.setBaseGas(111)
+            expect((await relayManager.baseGas()).toString()).to.eq("111")
+        })
+        it("setMinTransferAmount", async () => {
+            await relayManager.setMinTransferAmount(112)
+            expect((await relayManager.minTransferAmount()).toString()).to.eq("112")
+        })
+    })
+
+    describe("#Ownership", async () => {
+        it("should transfer ownership", async () => {
+            await relayManager.transferOwnership(bob)
+            await relayManager.acceptOwnership({ from: bob })
+            expect(await relayManager.owner()).to.eq(bob)
+            expectEvent(await relayManager.setAdminFee(1, { from: bob }), "AdminFeeChanged")
+        })
+        describe("reverts if", async () => {
+            it("non-owner call setMinTransferAmount", async () => {
+                await expectRevert(relayManager.setMinTransferAmount(1, { from: carol }), "RelayManagerETH: CALLER_NO_OWNER")
+            })
+            it("non-owner call setAdminFee", async () => {
+                await expectRevert(relayManager.setAdminFee(1, { from: carol }), "RelayManagerETH: CALLER_NO_OWNER")
+            })
+            it("non-owner call transferOwnership", async () => {
+                await expectRevert(relayManager.transferOwnership(bob, { from: carol }), "RelayManagerETH: CALLER_NO_OWNER")
+            })
+            it("call transferOwnership with zero address", async () => {
+                await expectRevert(relayManager.transferOwnership(constants.ZERO_ADDRESS, { from: bob }), "RelayManagerETH: INVALID_ADDRESS")
+            })
+            it("non owner call renounceOwnership", async () => {
+                await expectRevert(relayManager.renounceOwnership({ from: carol }), "RelayManagerETH: CALLER_NO_OWNER")
+            })
+            it("non new owner call acceptOwnership", async () => {
+                await relayManager.transferOwnership(alice, { from: bob })
+                await expectRevert(relayManager.acceptOwnership({ from: carol }), "RelayManagerETH: CALLER_NO_NEW_OWNER")
+                expectEvent(await relayManager.renounceOwnership({ from: bob }), "OwnershipTransferred")
+            })
+        })
+    })
+})
