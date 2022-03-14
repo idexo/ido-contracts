efd0f39980f6b196ded5de8b58cf17737976cfd4
Helder Sepulveda <heldersepu@gmail.com>
2022-03-14 14:05:37 -0400

    Add missing virtual

--- contracts/staking/StakePoolMultipleRewardsV1.sol
+++ contracts/staking/StakePoolMultipleRewardsV1.sol
@@ -225,7 +225,7 @@ contract StakePoolMultipleRewardsV1 is IStakePoolMultipleRewardsV1, StakeTokenMu
         address account,
         uint256 amount,
         uint256 timestamplock
-    ) internal nonReentrant {
+    ) internal virtual nonReentrant {
         uint256 depositedAt = block.timestamp;
         uint256 stakeId = _mint(account, amount, depositedAt, timestamplock);
         require(depositToken.transferFrom(account, address(this), amount), "StakePool#_deposit: TRANSFER_FAILED");
@@ -248,7 +248,7 @@ contract StakePoolMultipleRewardsV1 is IStakePoolMultipleRewardsV1, StakeTokenMu
         address account,
         uint256 stakeId,
         uint256 withdrawAmount
-    ) internal nonReentrant {
+    ) internal virtual nonReentrant {
         require(ownerOf(stakeId) == account, "StakePool#_withdraw: NO_STAKE_OWNER");
         _decreaseStakeAmount(stakeId, withdrawAmount);
         require(depositToken.transfer(account, withdrawAmount), "StakePool#_withdraw: TRANSFER_FAILED");
@@ -265,7 +265,7 @@ contract StakePoolMultipleRewardsV1 is IStakePoolMultipleRewardsV1, StakeTokenMu
         address account,
         address rewardTokenAddress,
         uint256 amount
-    ) internal {
+    ) internal virtual {
         rewardTokens[rewardTokenAddress].safeTransferFrom(account, address(this), amount);
         rewardDeposits[rewardTokenAddress].push(RewardDeposit({ operator: account, amount: amount, depositedAt: block.timestamp }));
 
@@ -276,7 +276,7 @@ contract StakePoolMultipleRewardsV1 is IStakePoolMultipleRewardsV1, StakeTokenMu
      * @dev Add new reward token.
      * @param rewardToken_ reward token address.
      */
-    function _addRewardToken(address rewardToken_) internal {
+    function _addRewardToken(address rewardToken_) internal virtual {
         rewardTokens[rewardToken_] = IERC20(rewardToken_);
     }
 }
