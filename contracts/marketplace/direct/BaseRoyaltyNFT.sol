3b714bcd59c1b53bbc38f896295421f0d86e0b66
Diogo Oliveira <dluks82@gmail.com>
2022-04-20 15:16:18 -0300

    some fixes dup declarations

--- contracts/marketplace/direct/BaseRoyaltyNFT.sol
+++ contracts/marketplace/direct/BaseRoyaltyNFT.sol
@@ -3,11 +3,11 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
-// import "./Error.sol";
-import "../lib/Operatorable.sol";
+import "../../lib/Error.sol";
+import "../../lib/Operatorable.sol";
 
 contract BaseRoyaltyNFT is ERC721Enumerable, ERC721URIStorage, Operatorable, Pausable {
     // Base token URI
