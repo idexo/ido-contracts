5cd023f27f74ef33ca700d33063ade73297e20b8
Helder Sepulveda <heldersepu@gmail.com>
2022-04-21 09:53:32 -0400

    Remove commented imports

--- contracts/marketplace/direct/DirectSale.sol
+++ contracts/marketplace/direct/DirectSale.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
-// import "./Error.sol";
 import "./RoyaltyNFT.sol";
 
 contract DirectSale is Ownable, Pausable {
