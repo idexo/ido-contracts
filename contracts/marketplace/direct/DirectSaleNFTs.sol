5f2bad17b8e9e7ec28801ff0fe2c828a232ac35e
Helder Sepulveda <heldersepu@gmail.com>
2022-04-21 19:43:05 -0400

    Use Ownable from our lib

--- contracts/marketplace/direct/DirectSaleNFTs.sol
+++ contracts/marketplace/direct/DirectSaleNFTs.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "../../lib/Ownable.sol";
 
 contract DirectSaleNFTs is Ownable {
     using SafeERC20 for IERC20;
