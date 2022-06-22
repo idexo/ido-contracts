69dfab4805a6978220fcda6d66100879cb4506f5
Helder Sepulveda <heldersepu@gmail.com>
2022-06-22 13:31:19 -0400

    Remove testToken KRED

--- test/kred.test.js
+++ test/kred.test.js
@@ -1,6 +1,5 @@
 const {testToken} = require('./kred');
 
-testToken('KRED', 'contracts/token/KRED.sol:KRED');
 testToken('CRED', 'contracts/token/CRED.sol:CRED');
 testToken('PIDO', 'contracts/token/PIDO.sol:PIDO');
 testToken('FIDO', 'contracts/token/FIDO.sol:FIDO');
