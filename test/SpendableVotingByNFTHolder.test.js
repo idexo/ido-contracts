5e5c35f9e6000f9a9f76202063c7ad6087320930
Diogo Lucas de Oliveira <dluks82@gmail.com>
2022-11-24 15:15:25 -0300

    start tests on SpendableVotingByNFTHolder

--- test/SpendableVotingByNFTHolder.test.js
+++ test/SpendableVotingByNFTHolder.test.js
@@ -10,17 +10,17 @@ const time = require("./helpers/time")
 const timeTraveler = require("ganache-time-traveler")
 
 contract("Voting", async (accounts) => {
-    let ido, erc20
+    let ido, usdt
     let voting, sPool1, sPool2, sPool3, sPool4
     const [alice, bob, carol] = accounts
 
     before(async () => {
         ido = await ERC20.new("Idexo Community", "IDO", { from: alice })
-        erc20 = await ERC20.new("USD Tether", "USDT", { from: alice })
-        sPool1 = await StakePool.new("Idexo Stake Token", "IDS", "", ido.address, erc20.address)
-        sPool2 = await StakePool.new("Idexo Stake Token", "IDS", "", ido.address, erc20.address)
-        sPool3 = await StakePool.new("Idexo Stake Token", "IDS", "", ido.address, erc20.address)
-        sPool4 = await StakePool.new("Idexo Stake Token", "IDS", "", ido.address, erc20.address)
+        usdt = await ERC20.new("USD Tether", "USDT", { from: alice })
+        sPool1 = await StakePool.new("Idexo Stake Token", "IDS", "", ido.address, usdt.address)
+        sPool2 = await StakePool.new("Idexo Stake Token", "IDS", "", ido.address, usdt.address)
+        sPool3 = await StakePool.new("Idexo Stake Token", "IDS", "", ido.address, usdt.address)
+        sPool4 = await StakePool.new("Idexo Stake Token", "IDS", "", ido.address, usdt.address)
         voting = await Voting.new(15, 15, [sPool1.address, sPool2.address])
     })
 
@@ -44,12 +44,49 @@ contract("Voting", async (accounts) => {
         })
     })
 
+    describe("#Fund Accounts", async () => {
+        it("should fund accounts with usdt", async () => {
+            await usdt.mint(alice, web3.utils.toWei(new BN(5000)))
+            await usdt.mint(bob, web3.utils.toWei(new BN(5000)))
+        })
+    })
+
+    describe("#Deposit Funds", async () => {
+        it("should deposit funds to this voting contract", async () => {
+            await usdt.balanceOf(alice).then((balance) => {
+                expect(balance.toString()).to.eq(web3.utils.toWei(new BN(5000)).toString())
+            })
+            await usdt.approve(voting.address, web3.utils.toWei(new BN(5000)), { from: alice })
+            await voting.depositFunds(usdt.address, web3.utils.toWei(new BN(5000)), { from: alice })
+
+            await usdt.balanceOf(voting.address).then((balance) => {
+                expect(balance.toString()).to.eq(web3.utils.toWei(new BN(5000)).toString())
+            })
+        })
+
+        describe("reverts if", async () => {
+            it("INSUFFICIENT_BALANCE", async () => {
+                await expectRevert(voting.depositFunds(usdt.address, web3.utils.toWei(new BN(5000)), { from: alice }), "INSUFFICIENT_BALANCE")
+            })
+            it("INSUFFICIENT_ALLOWANCE", async () => {
+                await expectRevert(voting.depositFunds(usdt.address, web3.utils.toWei(new BN(5000)), { from: bob }), "INSUFFICIENT_ALLOWANCE")
+            })
+        })
+    })
+
     describe("#Get", async () => {
         it("getReviewIds", async () => {
             expect(Number(await voting.getReviewIds(1))).to.eq(0)
         })
         it("getReview", async () => {
             await expectRevert(voting.getReview(1, 1), "INVALID_PROPOSAL")
+        })
+    })
+
+    describe("#Proposal", async () => {
+        it("create new proposal", async () => {
+            // voting.createProposal("Test Proposal 1", erc20.address)
+            // expect(Number(await voting.getReviewIds(1))).to.eq(0)
         })
     })
 })
