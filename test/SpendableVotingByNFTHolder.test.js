3abcf1a92a918b8a2dfac50063753c28716c4c45
Diogo Lucas de Oliveira <dluks82@gmail.com>
2022-11-24 21:52:27 -0300

    start test End proposal

--- test/SpendableVotingByNFTHolder.test.js
+++ test/SpendableVotingByNFTHolder.test.js
@@ -4,6 +4,7 @@ const ERC20 = artifacts.require("ERC20Mock")
 
 const { Contract } = require("@ethersproject/contracts")
 const { expect } = require("chai")
+const { duration } = require("./helpers/time")
 const { BN, constants, expectEvent, expectRevert } = require("@openzeppelin/test-helpers")
 const { toWei } = require("web3-utils")
 const time = require("./helpers/time")
@@ -12,7 +13,7 @@ const timeTraveler = require("ganache-time-traveler")
 contract("Voting", async (accounts) => {
     let ido, usdt
     let voting, sPool1, sPool2, sPool3, sPool4
-    const [alice, bob, carol] = accounts
+    const [alice, bob, carol, darren, payeeWallet] = accounts
 
     before(async () => {
         ido = await ERC20.new("Idexo Community", "IDO", { from: alice })
@@ -48,30 +49,35 @@ contract("Voting", async (accounts) => {
         it("should fund accounts with usdt", async () => {
             await usdt.mint(alice, web3.utils.toWei(new BN(5000)))
             await usdt.mint(bob, web3.utils.toWei(new BN(5000)))
+            await usdt.mint(carol, web3.utils.toWei(new BN(5000)))
         })
-    })
 
-    describe("#Mock Stakes", async () => {
-        before(async () => {
-            await ido.mint(alice, web3.utils.toWei(new BN(200000)))
-            await ido.approve(sPool1.address, web3.utils.toWei(new BN(200000)), { from: alice })
-            await ido.mint(bob, web3.utils.toWei(new BN(200000)))
-            await ido.approve(sPool2.address, web3.utils.toWei(new BN(200000)), { from: bob })
-            await ido.mint(carol, web3.utils.toWei(new BN(200000)))
-            await ido.approve(sPool1.address, web3.utils.toWei(new BN(200000)), { from: carol })
-        })
-        describe("##stakes", async () => {
-            it("should create mock stakes", async () => {
-                await sPool1.deposit(web3.utils.toWei(new BN(5200)), { from: alice })
-                await sPool1.getStakeInfo(1).then((res) => {
-                    expect(res[0].toString()).to.eq("5200000000000000000000")
+        describe("#Mock Stakes", async () => {
+            before(async () => {
+                await ido.mint(alice, web3.utils.toWei(new BN(200000)))
+                await ido.approve(sPool1.address, web3.utils.toWei(new BN(200000)), { from: alice })
+                await ido.mint(bob, web3.utils.toWei(new BN(200000)))
+                await ido.approve(sPool2.address, web3.utils.toWei(new BN(200000)), { from: bob })
+                await ido.mint(carol, web3.utils.toWei(new BN(200000)))
+                await ido.approve(sPool1.address, web3.utils.toWei(new BN(200000)), { from: carol })
+            })
+            describe("##stakes", async () => {
+                it("should create mock stakes", async () => {
+                    await sPool1.deposit(web3.utils.toWei(new BN(5200)), { from: alice })
+                    await sPool1.getStakeInfo(1).then((res) => {
+                        expect(res[0].toString()).to.eq("5200000000000000000000")
+                    })
+                    await sPool2.deposit(web3.utils.toWei(new BN(5200)), { from: bob })
+                    await sPool2.getStakeInfo(1).then((res) => {
+                        expect(res[0].toString()).to.eq("5200000000000000000000")
+                    })
+                    await sPool1.deposit(web3.utils.toWei(new BN(5200)), { from: carol })
+                    await sPool1.getStakeInfo(2).then((res) => {
+                        expect(res[0].toString()).to.eq("5200000000000000000000")
+                    })
+                    const aliceIDOBalance = await ido.balanceOf(alice)
+                    expect(aliceIDOBalance.toString()).to.eq("194800000000000000000000")
                 })
-                await sPool2.deposit(web3.utils.toWei(new BN(5200)), { from: bob })
-                await sPool2.getStakeInfo(1).then((res) => {
-                    expect(res[0].toString()).to.eq("5200000000000000000000")
-                })
-                const aliceIDOBalance = await ido.balanceOf(alice)
-                expect(aliceIDOBalance.toString()).to.eq("194800000000000000000000")
             })
         })
     })
@@ -90,7 +96,7 @@ contract("Voting", async (accounts) => {
         describe("reverts if", async () => {
             it("INSUFFICIENT_FUNDS", async () => {
                 await expectRevert(
-                    voting.createProposal("Test Proposal 1", alice, web3.utils.toWei(new BN(1)), usdt.address, 1, { from: alice }),
+                    voting.createProposal("Test Proposal 1", payeeWallet, web3.utils.toWei(new BN(1)), usdt.address, 1, { from: alice }),
                     "INSUFFICIENT_FUNDS"
                 )
             })
@@ -121,7 +127,7 @@ contract("Voting", async (accounts) => {
 
         describe("#Create proposal", async () => {
             it("create new proposal", async () => {
-                voting.createProposal("Test Proposal 1", alice, web3.utils.toWei(new BN(1)), usdt.address, 1, { from: alice })
+                voting.createProposal("Test Proposal 1", payeeWallet, web3.utils.toWei(new BN(1)), usdt.address, 1, { from: alice })
             })
         })
 
@@ -134,10 +140,10 @@ contract("Voting", async (accounts) => {
 
         describe("#Vote proposal", async () => {
             it("should vote proposal #1", async () => {
-                await voting.voteProposal(1, 1, { from: alice })
+                await voting.voteProposal(1, 2, { from: alice })
                 const proposal = await voting.getProposal(1)
                 await voting.getProposal(1).then((proposal) => {
-                    expect(proposal.options[0]["votes"]).to.eq("1")
+                    expect(proposal.options[1]["votes"]).to.eq("1")
                 })
             })
 
@@ -146,7 +152,7 @@ contract("Voting", async (accounts) => {
                     await expectRevert(voting.voteProposal(1, 1, { from: alice }), "ALREADY_VOTED")
                 })
                 it("NOT_NFT_HOLDER", async () => {
-                    await expectRevert(voting.voteProposal(1, 1, { from: carol }), "NOT_NFT_HOLDER")
+                    await expectRevert(voting.voteProposal(1, 1, { from: darren }), "NOT_NFT_HOLDER")
                 })
                 it("INVALID_OPTION", async () => {
                     await expectRevert(voting.voteProposal(1, 5, { from: bob }), "INVALID_OPTION")
@@ -187,6 +193,42 @@ contract("Voting", async (accounts) => {
                 it("PROPOSAL_VOTE_OPEN", async () => {
                     await sPool1.transferFrom(bob, alice, 1, { from: bob })
                     await expectRevert(voting.createReview(1, "First proposal id #1 review", { from: alice }), "PROPOSAL_VOTE_OPEN")
+                })
+                it("REJECTED_PROPOSAL", async () => {
+                    let snapShot = await timeTraveler.takeSnapshot()
+                    await timeTraveler.advanceTime(duration.days(20))
+                    await voting.endProposalVote(1, { from: alice })
+                    await expectRevert(voting.createReview(1, "First proposal id #1 review", { from: alice }), "REJECTED_PROPOSAL")
+                    await timeTraveler.revertToSnapshot(snapShot["result"])
+                })
+            })
+
+            // it("should create review", async () => {
+            //     await timeTraveler.advanceTime(duration.days(20))
+            //     await voting.createReview(1, "First proposal id #1 review", { from: alice })
+            // })
+        })
+
+        describe("#End Proposal", async () => {
+            describe("reverts if", async () => {
+                it("OPEN_FOR_VOTE", async () => {
+                    await expectRevert(voting.endProposalVote(1, { from: alice }), "OPEN_FOR_VOTE")
+                })
+                it("ALREADY_ENDED", async () => {
+                    let snapShot = await timeTraveler.takeSnapshot()
+                    await timeTraveler.advanceTime(duration.days(20))
+                    await voting.endProposalVote(1, { from: alice })
+                    await expectRevert(voting.endProposalVote(1, { from: alice }), "ALREADY_ENDED")
+                    await timeTraveler.revertToSnapshot(snapShot["result"])
+                })
+                it("INSUFFICIENT_FUNDS_CALL_ADMIN", async () => {
+                    let snapShot = await timeTraveler.takeSnapshot()
+                    await voting.voteProposal(1, 1, { from: bob })
+                    await voting.voteProposal(1, 1, { from: carol })
+                    await timeTraveler.advanceTime(duration.days(20))
+                    voting.sweep(usdt.address, carol, web3.utils.toWei(new BN(5000)))
+                    await expectRevert(voting.endProposalVote(1, { from: alice }), "INSUFFICIENT_FUNDS_CALL_ADMIN")
+                    await timeTraveler.revertToSnapshot(snapShot["result"])
                 })
                 // it("REJECTED_PROPOSAL", async () => {
                 //     await expectRevert(voting.voteProposal(1, 5, { from: bob }), "REJECTED_PROPOSAL")
