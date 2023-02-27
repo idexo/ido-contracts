8a6e1d86cb3e2c0928732bd2a4c087616baed6e7
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-02-27 17:23:33 -0300

    WrappedTokenCapped

---
+++ contracts/token/WrappedTokenCapped.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+import "../interfaces/IWrappedToken.sol";
+
+contract WrappedTokenCapped is IWrappedToken, ERC20Permit, Ownable2Step {
+    address public relayer;
+    uint256 public immutable cap;
+
+    event RelayerAddressChanged(address indexed relayer);
+
+    constructor(string memory wTokenName, string memory wTokenSymbol, uint256 wTokenCap) ERC20(wTokenName, wTokenSymbol) ERC20Permit(wTokenName) {
+        cap = wTokenCap * 1 ether;
+    }
+
+    /**************************|
+    |          Setters         |
+    |_________________________*/
+
+    /**
+     * @dev Set relayer address
+     * Only owner can call
+     */
+    function setRelayer(address newRelayer) external override onlyOwner {
+        require(newRelayer != address(0), "WIDO: NEW_RELAYER_ADDRESS_INVALID");
+        relayer = newRelayer;
+
+        emit RelayerAddressChanged(newRelayer);
+    }
+
+    /************************|
+    |          Token         |
+    |_______________________*/
+
+    /**
+     * @dev Mint new WIDO
+     * Only relayer can call
+     */
+    function mint(address account, uint256 amount) external override {
+        require(_msgSender() == relayer, "WIDO: CALLER_NO_RELAYER");
+        require(totalSupply() + amount <= cap, "WIDO: AMOUNT_EXCEEDS_CAP");
+        _mint(account, amount);
+    }
+
+    /**
+     * @dev Burn WIDO
+     * Only relayer can call
+     */
+    function burn(address account, uint256 amount) external override {
+        require(_msgSender() == relayer, "WIDO: CALLER_NO_RELAYER");
+        _burn(account, amount);
+    }
+
+    /**
+     * @dev Get chain id.
+     */
+    function getChainId() public view override returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+}
